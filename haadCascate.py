dcbe27e7f9b9a800251bc9c9e72fae9ca20d0020
FelipeRomeroPachecoSegundo <92553932+FelipeRomeroPachecoSegundo@users.noreply.github.com>
2022-08-10 16:09:58 -0300

    Update haadCascate.py

--- haadCascate.py
+++ haadCascate.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+from __future__ import print_function
 import cv2 as cv
 import argparse
 
@@ -8,20 +9,19 @@ def detectAndDisplay(frame, face_cascade, eyes_cascade):
     #-- Detect faces
     faces = face_cascade.detectMultiScale(frame_gray)
     for (x,y,w,h) in faces:
-        #center = (x + w//2, y + h//2)
         frame =  cv.rectangle(frame, (x,y), (x+w,y+h),(255,0,0),2)
-        faceROI = frame_gray[y:y+h,x:x+w]
-        #-- In each face, detect eyes
+        faceROI = frame_gray[y:y+h,x:x+w] 
         eyes = eyes_cascade.detectMultiScale(faceROI)
+        cv.putText(frame,"ROSTO",(x+w//2+1,y+h+25),cv.FONT_HERSHEY_DUPLEX,1,(255,0,0))
         for (x2,y2,w2,h2) in eyes:
             eye_center = (x + x2 + w2//2, y + y2 + h2//2)
-            #radius = int(round((w2 + h2)*0.25))
             frame = cv.rectangle(frame, (x+x2,y+y2), (x+x2+w2,y+y2+h2),(0,255,0),2)
+            cv.putText(frame,"OLHO",(x+x2+w2//2+1,y+y2+h2+25),cv.FONT_HERSHEY_DUPLEX,1,(0,255,0))
     cv.imshow('Capture - Face detection', frame)
 def goDetect(camera):
     parser = argparse.ArgumentParser(description='Code for Cascade Classifier tutorial.')
-    parser.add_argument('--face_cascade', help='Path to face cascade.', default='data/haarcascades/haarcascade_frontalface_alt2.xml')
-    parser.add_argument('--eyes_cascade', help='Path to eyes cascade.', default='data/haarcascades/haarcascade_eye_tree_eyeglasses.xml')
+    parser.add_argument('--face_cascade', help='Path to face cascade.', default=cv.samples.findFile('haarcascade_frontalface_alt2.xml'))
+    parser.add_argument('--eyes_cascade', help='Path to eyes cascade.', default=cv.samples.findFile('haarcascade_eye_tree_eyeglasses.xml'))
     parser.add_argument('--camera', help='Camera divide number.', type=int, default=0)
     args = parser.parse_args()
     face_cascade_name = args.face_cascade
